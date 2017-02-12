088b41454ed13d3a72f356b7357bdae026cbfd69
Amy Zhang <axz@mit.edu>
2017-02-11 20:23:36 -0500

    add models

--- recommender/website/models.py
+++ recommender/website/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    book_id = models.CharField(primary_key=True, max_length=50)
+    book_title = models.CharField(max_length=200)
+    book_author = models.CharField(max_length=200)
+    book_pub_date = models.CharField(max_length=40)
+    book_publisher = models.CharField(max_length=100)
+    description = models.TextField()
+    pagecount = models.IntegerField()
+    
+    def __unicode__(self):
+        return self.book_title
+
+class Reviews(models.Model):
+    book_id = models.ForeignKey('Book')
+    body = models.TextField()
+    rating = models.IntegerField()
+    url = models.CharField(max_length=100)
+
+class Shelves(models.Model):
+    book_id = models.ForeignKey('Book')
+    shelf = models.CharField(max_length=200)
+    people = models.IntegerField()
+    
+    def __unicode__(self):
+        return self.shelf
+    
+class Categories(models.Model):
+    book_id = models.ForeignKey('Book')
+    category = models.CharField(max_length=100)
+    
+    def __unicode__(self):
+        return self.category
+       
+    
+    
