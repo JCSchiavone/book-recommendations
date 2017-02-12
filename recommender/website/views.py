46635e3f67b46c6f8f4076ac6529af560034cb69
Amy Zhang <axz@mit.edu>
2017-02-12 12:17:22 -0500

    encoding

--- recommender/website/views.py
+++ recommender/website/views.py
@@ -18,7 +18,7 @@ def bookrec(request):
     data = request.GET.get('id')
     book = Book.objects.get(book_id=data)
     
-    book.description = book.description.encode("utf8")
+    book.description = book.description.encode('latin-1').decode('utf-8')
     
     tags = Shelves.objects.filter(book_id=data).order_by('-people')
     if len(tags) > 30:
